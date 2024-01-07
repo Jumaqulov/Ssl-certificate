fb977d5b9c9c433edd5b2e950c871322a492329c
Avazbek <avazjonjumoqulov@gmail.com>
2024-01-07 23:33:15 +0500

    bir nimalar qo'shildi

---
+++ public/data/ca_brands.js
@@ -0,0 +1,14 @@
+const menu = [
+    {
+        data: ['Basic OV (FLEX)', 'Basic EV', 'Wildcard SSL', 'Wildcard SSL']
+    },
+    {
+        data: ['90-day Trial SSL', 'Secure Domain SSL (FLEX)', 'Domain SSL', 'Multi-Domain (FLEX)', 'Wildcard SSL','Wildcard SSL']
+    },
+    {
+        data: [],
+    },
+    {
+        data: [],
+    },
+]
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-function App() {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './assets/styles/main.css';
+import svg1 from './assets/svg/1.svg';
+
+export default function App() {
+  const [jsonData, setJsonData] = useState(null);
+
+  const url = '/data/ca_brands.js '
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setJsonData(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Xatolik:', error);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      
+    <div>
+      <h1>JSON Fayl Ma'lumotlari</h1>
+      {jsonData ? (
+        <p>salom</p>
+      )
+        : (
+          <p>Ma'lumotlar yuklanmoqda...</p>
+        )}
     </div>
   );
 }
-
-export default App;
