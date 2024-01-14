45350075886cf8cbde889a35f9f0a954dfa73623
Avazbek <avazjonjumoqulov@gmail.com>
2024-01-14 22:48:03 +0500

    fdsf

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import Navbar from '../Components/Navbar/index'
 import { Outlet } from 'react-router-dom'
+import Header from '../Components/Header/Header'
 
 
 export default function Layout() {
   return (
     <Fragment>
       <div className="wrapper">
+        <Header/>
         <Navbar />
         <Outlet />
       </div>
