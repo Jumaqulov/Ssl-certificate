f75eee289c01d1fc62576436c0169426c3a8df84
Avazbek Jumaqulov <a.jumaqulov@starlab.uz>
2024-01-16 15:43:11 +0500

    footer-finished

---
+++ src/Components/Footer/FooterBottom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function FooterBottom() {
+    return (
+        <div className='footer-bottom'>
+            <div className="wrapper">
+                <p>© 2009-2023 Copyright Digicert Ireland Limited. All Rights reserved</p>
+            </div>
+        </div>
+    )
+}
--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import FooterTop from './FooterTop'
 import FooterMiddle from './FooterMiddle'
+import FooterBottom from './FooterBottom'
 
 export default function Footer() {
   return (
     <div className='footer'>
       <FooterTop />
       <FooterMiddle />
+      <FooterBottom/>
     </div>
   )
 }
