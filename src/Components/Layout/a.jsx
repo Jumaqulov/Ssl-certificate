92be4564d9151e51f4cf6ac5dbac1dc1bc27dd85
Avazbek Jumaqulov <a.jumaqulov@starlab.uz>
2024-01-19 17:33:23 +0500

    Carousel tugadi

--- src/Components/Layout/a.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Acomponent() {
-  return (
-    <div className='a-com'>a</div>
-  )
-}
--- src/Components/Layout/b.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Bcomponent() {
-  return (
-    <div className='b-com'>b</div>
-  )
-}
