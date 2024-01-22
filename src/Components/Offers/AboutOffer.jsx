25e591af590ec3911c7007905a5e222fa8d542a5
Avazbek Jumaqulov <a.jumaqulov@starlab.uz>
2024-01-22 14:41:39 +0500

    home page 100% done

---
+++ src/Components/Offers/AboutOffer.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export default function AboutOffer() {
+    return (
+        <div className='about-offer'>
+            <div>
+                <h3>Trusted SSL Certificates</h3>
+                <p className='d-flex view-lg'>
+                    <span>
+                        <svg className='platinium-svg' viewBox='0 0 24 32'>
+                            <path
+                                d="M17.417 22.686h-.06l-1.342-.05-.995.9a1.691 1.691 0 0 1-1.774.312l-1.243-.508-1.242.508a1.691 1.691 0 0 1-1.774-.312l-.996-.9-1.34.05H6.59c-.63 0-1.202-.345-1.497-.904l-.626-1.188-.05-.017v10.485c0 .63.663 1.04 1.225.755l5.986-3.006a.86.86 0 0 1 .761 0l5.976 3.01a.845.845 0 0 0 1.226-.755V20.58l-.051.016-.627 1.189c-.29.555-.866.9-1.496.9v.001z">
+                            </path>
+                            <path
+                                d="M23.552 12.059a.848.848 0 0 0 0-.9l-.715-1.138a.85.85 0 0 1-.111-.627l.284-1.313a.847.847 0 0 0-.308-.847l-1.06-.822a.848.848 0 0 1-.318-.552l-.186-1.33a.852.852 0 0 0-.58-.691l-1.276-.414a.847.847 0 0 1-.487-.41l-.626-1.188a.847.847 0 0 0-.749-.45h-.03l-1.34.051h-.031a.843.843 0 0 1-.57-.22l-.994-.9a.856.856 0 0 0-.57-.22.798.798 0 0 0-.317.064l-1.243.507a.8.8 0 0 1-.636 0L10.446.152a.84.84 0 0 0-.887.156l-.995.9a.843.843 0 0 1-.57.22h-.03l-1.34-.05h-.03a.847.847 0 0 0-.75.45L5.22 3.015a.835.835 0 0 1-.488.41l-1.276.413a.848.848 0 0 0-.58.69L2.69 5.86a.849.849 0 0 1-.319.553l-1.073.822a.847.847 0 0 0-.308.847l.284 1.313a.832.832 0 0 1-.111.627l-.715 1.137a.848.848 0 0 0 0 .9l.715 1.138a.85.85 0 0 1 .111.627L.99 15.137a.847.847 0 0 0 .308.847l1.06.823a.848.848 0 0 1 .318.551l.186 1.331c.044.322.271.59.58.69l.967.312.312.102c.21.067.386.216.487.41l.627 1.188c.145.277.437.45.748.45h.03l1.341-.05h.03c.21 0 .414.077.57.22l.995.9c.159.142.362.22.569.22.108 0 .216-.02.318-.064l1.242-.508a.862.862 0 0 1 .319-.065.8.8 0 0 1 .318.065l1.242.508a.84.84 0 0 0 .887-.156l.996-.9a.844.844 0 0 1 .569-.22h.03l1.34.05h.031a.847.847 0 0 0 .748-.45l.627-1.189a.835.835 0 0 1 .487-.41l.312-.1.968-.312a.847.847 0 0 0 .58-.691l.185-1.33a.848.848 0 0 1 .318-.552l1.06-.823a.847.847 0 0 0 .308-.847l-.284-1.313a.832.832 0 0 1 .112-.627l.72-1.137.001-.001zm-3.363 3.011a8.79 8.79 0 0 1-1.903 2.824 8.858 8.858 0 0 1-6.284 2.6 8.835 8.835 0 0 1-3.46-.698 8.82 8.82 0 0 1-2.823-1.903 8.899 8.899 0 0 1-1.903-2.823 8.837 8.837 0 0 1-.698-3.46c0-1.199.234-2.364.698-3.46.447-1.06 1.087-2.008 1.903-2.824a8.858 8.858 0 0 1 6.283-2.6c1.199 0 2.364.233 3.46.697 1.06.447 2.008 1.087 2.824 1.903a8.858 8.858 0 0 1 2.6 6.284 8.836 8.836 0 0 1-.697 3.46z">
+                            </path>
+                            <path
+                                d="M12 3.737a7.872 7.872 0 1 0 0 15.744 7.872 7.872 0 0 0 0-15.744zm3.504 11.518a.845.845 0 0 1-1.246.91L12 14.886l-2.262 1.28c-.633.36-1.398-.2-1.246-.91l.542-2.553-1.767-1.595-.196-.179a.848.848 0 0 1 .463-1.47l2.573-.314 1.121-2.4a.847.847 0 0 1 1.534 0l1.12 2.4 2.573.315a.847.847 0 0 1 .464 1.47l-.196.178-1.767 1.595.548 2.553v-.001z">
+                            </path>
+                        </svg>
+                    </span>
+                    <span>
+                        GOGETSSL® is a well-known and trusted brand on the market with more than 101,500 customers from 180+ countries. We sell digital certificates from leading certificate authorities for more than 10 years. Our store is a great place to find the <a className='small-link' href="/ssl-certificates">cheapest SSL certificates</a> to protect any website, application and online transactions. Our certificates issued by leading providers like Digicert, Geotrust, RapidSSL, Thawte, and Sectigo.
+                    </span>
+                </p>
+                <p className='view-sm'>
+                    <span className='d-flex'>
+                        <span>
+                            <svg className='platinium-svg' viewBox='0 0 24 32'>
+                                <path
+                                    d="M17.417 22.686h-.06l-1.342-.05-.995.9a1.691 1.691 0 0 1-1.774.312l-1.243-.508-1.242.508a1.691 1.691 0 0 1-1.774-.312l-.996-.9-1.34.05H6.59c-.63 0-1.202-.345-1.497-.904l-.626-1.188-.05-.017v10.485c0 .63.663 1.04 1.225.755l5.986-3.006a.86.86 0 0 1 .761 0l5.976 3.01a.845.845 0 0 0 1.226-.755V20.58l-.051.016-.627 1.189c-.29.555-.866.9-1.496.9v.001z">
+                                </path>
+                                <path
+                                    d="M23.552 12.059a.848.848 0 0 0 0-.9l-.715-1.138a.85.85 0 0 1-.111-.627l.284-1.313a.847.847 0 0 0-.308-.847l-1.06-.822a.848.848 0 0 1-.318-.552l-.186-1.33a.852.852 0 0 0-.58-.691l-1.276-.414a.847.847 0 0 1-.487-.41l-.626-1.188a.847.847 0 0 0-.749-.45h-.03l-1.34.051h-.031a.843.843 0 0 1-.57-.22l-.994-.9a.856.856 0 0 0-.57-.22.798.798 0 0 0-.317.064l-1.243.507a.8.8 0 0 1-.636 0L10.446.152a.84.84 0 0 0-.887.156l-.995.9a.843.843 0 0 1-.57.22h-.03l-1.34-.05h-.03a.847.847 0 0 0-.75.45L5.22 3.015a.835.835 0 0 1-.488.41l-1.276.413a.848.848 0 0 0-.58.69L2.69 5.86a.849.849 0 0 1-.319.553l-1.073.822a.847.847 0 0 0-.308.847l.284 1.313a.832.832 0 0 1-.111.627l-.715 1.137a.848.848 0 0 0 0 .9l.715 1.138a.85.85 0 0 1 .111.627L.99 15.137a.847.847 0 0 0 .308.847l1.06.823a.848.848 0 0 1 .318.551l.186 1.331c.044.322.271.59.58.69l.967.312.312.102c.21.067.386.216.487.41l.627 1.188c.145.277.437.45.748.45h.03l1.341-.05h.03c.21 0 .414.077.57.22l.995.9c.159.142.362.22.569.22.108 0 .216-.02.318-.064l1.242-.508a.862.862 0 0 1 .319-.065.8.8 0 0 1 .318.065l1.242.508a.84.84 0 0 0 .887-.156l.996-.9a.844.844 0 0 1 .569-.22h.03l1.34.05h.031a.847.847 0 0 0 .748-.45l.627-1.189a.835.835 0 0 1 .487-.41l.312-.1.968-.312a.847.847 0 0 0 .58-.691l.185-1.33a.848.848 0 0 1 .318-.552l1.06-.823a.847.847 0 0 0 .308-.847l-.284-1.313a.832.832 0 0 1 .112-.627l.72-1.137.001-.001zm-3.363 3.011a8.79 8.79 0 0 1-1.903 2.824 8.858 8.858 0 0 1-6.284 2.6 8.835 8.835 0 0 1-3.46-.698 8.82 8.82 0 0 1-2.823-1.903 8.899 8.899 0 0 1-1.903-2.823 8.837 8.837 0 0 1-.698-3.46c0-1.199.234-2.364.698-3.46.447-1.06 1.087-2.008 1.903-2.824a8.858 8.858 0 0 1 6.283-2.6c1.199 0 2.364.233 3.46.697 1.06.447 2.008 1.087 2.824 1.903a8.858 8.858 0 0 1 2.6 6.284 8.836 8.836 0 0 1-.697 3.46z">
+                                </path>
+                                <path
+                                    d="M12 3.737a7.872 7.872 0 1 0 0 15.744 7.872 7.872 0 0 0 0-15.744zm3.504 11.518a.845.845 0 0 1-1.246.91L12 14.886l-2.262 1.28c-.633.36-1.398-.2-1.246-.91l.542-2.553-1.767-1.595-.196-.179a.848.848 0 0 1 .463-1.47l2.573-.314 1.121-2.4a.847.847 0 0 1 1.534 0l1.12 2.4 2.573.315a.847.847 0 0 1 .464 1.47l-.196.178-1.767 1.595.548 2.553v-.001z">
+                                </path>
+                            </svg>
+                        </span>
+                        <p>GOGETSSL® is a well-known and trusted brand on the market with more than 101,500 customers</p>
+                    </span>
+                    <span>
+                        from 180+ countries. We sell digital certificates from leading certificate authorities for more than 10 years. Our store is a great place to find the <a className='small-link' href="/ssl-certificates">cheapest SSL certificates</a> to protect any website, application and online transactions. Our certificates issued by leading providers like Digicert, Geotrust, RapidSSL, Thawte, and Sectigo.
+                    </span>
+                </p>
+                <p className='about-offer-p'>We have domain validated, organization validation, extended validation, wildcard SSL certificates and of course multiple domains certificates available. Every certificate comes with the green lock in the address bar and website ranking improvement in Google Search engine. With EV certificates you get even more instant trust via green address bar with your verified company name.</p>
+                <p className='about-offer-p'>SSL protocol is one the most powerful way to protect your traffic against hackers and criminals. Remember, losing sensitive data of your customers means their losing trust!</p>
+            </div>
+            <div>
+                <a className='big-link big-link-1' href="/reseller-program">Become Reseller</a>
+                <p>Sell cheap SSL to customers using our automation platform. Switch to us and get better pricing, exclusive conditions with monthly invoicing. Sign up for <a className='small-link sm-link-1' href="/reseller-program">Premium Reseller </a>program with no setup fee, minimal annual commitment or contract requirements.</p>
+                <p>Featured WHMCS SSL addon, HostbillApp and API modules available for free. Comfortable web management control platform.</p>
+                <p>Customers of our partners are not our customers, that is why we keep system White Labeled as much as we can and never contact anyone directly.</p>
+            </div>
+            <div>
+                <a className='big-link big-link-2' href="/compare-dv-ssl">SSL Comparison</a>
+                <p>Our security experts spent weeks preparing a full and most detailed <a className='small-link sm-link-2' href="/compare-dv-ssl">comparison</a> tool available today. Compare digital certificates by price, validation type, warranty, mobile compatibility and end-user trust level. More than twenty-five different criteria available during the detailed comparison.</p>
+                <p>It is free and suitable for any user not familiar with the technology. Use it for quick and simple selection of the most suitable certificate for your needs.</p>
+                <p>Our sales team is happy to assist if you have any doubts during the selection of a proper SSL certificate.</p>
+            </div>
+        </div>
+    )
+}
