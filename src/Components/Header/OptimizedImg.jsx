1ee9bfae2083797a22748a2c34eca56afcb710c8
Avazbek19 <avazjonjumoqulov@gmail.com>
2024-05-31 10:30:39 +0500

    headerga optimized qo'shildi

--- src/Components/Header/OptimizedImg.jsx
+++ src/Components/Header/OptimizedImg.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+const preloadImage = (url) => {
+    const img = new Image();
+    img.src = url;
+};
+
 
 export default function OptimizedImg({ src, alt, width, height }) {
+    useEffect(() => {
+        preloadImage(src);
+    }, [src]);
     return (
         <img src={src} alt={alt} width={width} height={height} loading="eager" />
     )
