4af1d029e27186052d70e974f09e71ce67c1431f
Avazbek19 <avazjonjumoqulov@gmail.com>
2024-06-05 17:36:16 +0500

    404 page o'zgartirildi

--- src/Components/404Page/NotFound.jsx
+++ src/Components/404Page/NotFound.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { TbError404 } from "react-icons/tb";
 
 export default function NotFound() {
     document.title = 'Страница не найдена';
+
+    useEffect(() => {
+        if (window.dataLayer) {
+            window.dataLayer.push({
+                event: '404_pageview',
+                page: {
+                    url: window.location.pathname,
+                    title: document.title
+                }
+            });
+        }
+    }, []);
 
     return (
         <div className='certificates'>
