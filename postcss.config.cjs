bdae791cd4fe19279561fd408160da8abe244754
Avazbek19 <avazjonjumoqulov@gmail.com>
2024-06-07 11:29:18 +0500

    post css konfig o'zgartirildi

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,18 @@
 module.exports = {
     plugins: [
-      require('autoprefixer'),
-      require('@fullhuman/postcss-purgecss')({
-        content: [
-          './src/**/*.{js,jsx}', 
-          './public/index.html'
-        ],
-        css: ['./src/styles.css'],
-        defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-        safelist: [],
-        blocklist: [], 
-      })
+        require('autoprefixer'),
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './src/**/*.{js,jsx}',
+                './public/index.html'
+            ],
+            css: ['./src/**/*.css'], // Bu yerda barcha CSS fayllarni ko'rsating
+            defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
+            safelist: {
+                standard: [], // Bu yerda saqlanishi kerak bo'lgan sinflarni qo'shing
+                deep: [], // Bu yerda chuqurroq tekshiriladigan sinflarni qo'shing
+                greedy: [] // Bu yerda har qanday joyda topilishi mumkin bo'lgan sinflarni qo'shing
+            }
+        })
     ]
-  }
-  
+}
