b8da314067ce00d95d8d8c50c54192f2c6f7c293
Avazbek19 <avazjonjumoqulov@gmail.com>
2024-06-07 11:19:51 +0500

    konfig qo'shildi

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,16 @@
 module.exports = {
     plugins: [
-        require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './src/**/*.js',
-                './src/**/*.jsx',
-                './public/index.html'
-            ],
-            css: ['./src/styles.css'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
+      require('autoprefixer'),
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+          './src/**/*.{js,jsx}', 
+          './public/index.html'
+        ],
+        css: ['./src/styles.css'],
+        defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
+        safelist: [],
+        blocklist: [], 
+      })
     ]
-}
+  }
+  
