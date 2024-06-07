3863e1e8740571c48f80651c1b53b0fab94073c9
Avazbek19 <avazjonjumoqulov@gmail.com>
2024-06-07 12:07:47 +0500

    post css o'zgardi

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -9,10 +9,13 @@ module.exports = {
             css: ['./src/**/*.css'],
             defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
             safelist: {
-                standard: [],
-                deep: [], 
-                greedy: [] 
+                standard: [], // Bu yerda saqlanishi kerak bo'lgan sinflarni qo'shing
+                deep: [], // Bu yerda chuqurroq tekshiriladigan sinflarni qo'shing
+                greedy: [] // Bu yerda har qanday joyda topilishi mumkin bo'lgan sinflarni qo'shing
             }
-        })
+        }),
+        require('cssnano')({
+            preset: 'default',
+        }),
     ]
 }
