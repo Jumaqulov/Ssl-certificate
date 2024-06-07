af09b2377805493d86f412808da4b0e02ac74d2e
Avazbek19 <avazjonjumoqulov@gmail.com>
2024-06-07 17:38:15 +0500

    webpack o'zgardi

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,6 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader' // Bu yerda postcss-loader ni qo'shing
-                ]
-            }
-        ]
+    mode: 'production',
+    optimization: {
+        usedExports: true,
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
-    ]
-}
+};
