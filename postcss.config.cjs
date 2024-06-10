d02c46cdd34b6ac5c0607b49f281d121aa81e5da
Avazbek19 <avazjonjumoqulov@gmail.com>
2024-06-10 09:35:14 +0500

    configlar o'chirib qo'yildi

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,18 +1,18 @@
-module.exports = {
-    plugins: [
-        require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss')({
-            content: ['./src/**/*.html', './src/**/*.js'],
-            css: ['./src/**/*.css'],
-            defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-            safelist: {
-                standard: [], // Bu yerda saqlanishi kerak bo'lgan sinflarni qo'shing
-                deep: [], // Bu yerda chuqurroq tekshiriladigan sinflarni qo'shing
-                greedy: [] // Bu yerda har qanday joyda topilishi mumkin bo'lgan sinflarni qo'shing
-            }
-        }),
-        require('cssnano')({
-            preset: 'default',
-        }),
-    ]
-}
+// module.exports = {
+//     plugins: [
+//         require('autoprefixer'),
+//         require('@fullhuman/postcss-purgecss')({
+//             content: ['./src/**/*.html', './src/**/*.js'],
+//             css: ['./src/**/*.css'],
+//             defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
+//             safelist: {
+//                 standard: [], // Bu yerda saqlanishi kerak bo'lgan sinflarni qo'shing
+//                 deep: [], // Bu yerda chuqurroq tekshiriladigan sinflarni qo'shing
+//                 greedy: [] // Bu yerda har qanday joyda topilishi mumkin bo'lgan sinflarni qo'shing
+//             }
+//         }),
+//         require('cssnano')({
+//             preset: 'default',
+//         }),
+//     ]
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-    mode: 'production',
-    optimization: {
-        usedExports: true,
-    },
-};
+// module.exports = {
+//     mode: 'production',
+//     optimization: {
+//         usedExports: true,
+//     },
+// };
